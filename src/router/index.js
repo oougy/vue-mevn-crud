563c49775400f1adac97b58d04a4f4002f247984
oougy <putthi.n@gmail.com>
2022-07-16 22:27:45 +0700

    make it better

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,7 @@ import CreateComponent from "@/components/CreateComponent.vue";
 import ListComponent from "@/components/ListComponent.vue";
 import EditComponent from "@/components/EditComponent.vue";
 import ShowComponent from "@/components/ShowComponent.vue";
-import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://oougy:nodaorgi@cluster1.gxp1rfn.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
 const routes = [
 {
     path: "/",
